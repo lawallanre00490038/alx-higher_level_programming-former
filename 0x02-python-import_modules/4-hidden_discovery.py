8141e6ef1676628da61a03ae837f8b6e6f0f98cc
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2022-09-27 01:11:24 -0700

    test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "_":
-            print("{:s}".format(s))
+    for name in dir(hidden_4):
+        if not name.startswith("_"):
+            print("{:s}".format(name))
