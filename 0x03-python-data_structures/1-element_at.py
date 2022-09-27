7353af41612f0004c783664bbb3c245b1e46505c
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-09-27 19:05:57 +0100

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx => len(my_list):
-        return None
-    else:
-        return (my_list[idx])
+    return (my_list[idx] if 0 <= idx < len(my_list) else None)
