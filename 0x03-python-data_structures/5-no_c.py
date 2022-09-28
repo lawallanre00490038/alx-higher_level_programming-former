733404b6bc793d9c27076e2c9f25042ad373ddd5
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2022-09-28 04:49:26 -0700

    test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for elem in my_string:
-        if elem == "c" or elem == "C":
-            continue
-        new_string += elem
+        if elem != "c" and elem != "C":
+            new_string += elem
+    return new_string
