676a38f17d77c9aacc5a9b6455f311eec03824a6
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2022-10-15 01:02:16 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-This is a modeule that import a function called 
-matrix_divided from which series of test is carried out
+This is the "2-matrix_divided" module.
+
+The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
 """
 def matrix_divided(matrix, div):
-    """
-    This is a modeule that import a function called 
-    matrix_divided from which series of test is carried out
-    """
+    """Divides all elements in the matrix by div"""
     for i in range(2):
         for j in range(3):
             if not isinstance(matrix[i][j], (int, float)):
