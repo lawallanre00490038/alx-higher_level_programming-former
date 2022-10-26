8aba2014ed84973d0cd79810751f06029f4e3c8a
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2022-10-26 02:56:46 -0700

    test

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+this si the funnction that print all the function of the function
+
+"""
+
+def lookup(obj):
+    """the function that prints the info of the functions"""
+    return dir(obj)
