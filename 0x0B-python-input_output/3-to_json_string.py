67f74a6bdbcb72cd397427fe6cea64a8655951dd
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2022-10-26 01:17:33 -0700

    test

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this is the function that returns a json rep of a string
+"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """this is the function"""
+    return json.dumbs(my_obj)
