cfb6b5d61cff47e5f20b1ce2826c042891708362
Lawal Olanrewaju <lawallanre.abbahouse@gmail.com>
2022-10-26 00:57:07 -0700

    test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,15 +4,6 @@ this is the function that writes a texts into a text file
 """
 def write_file(filename="", text=""):
     """this is the fuunction in the module"""
-    with open(filename, 'w') as f:
-        f.write(text)
-
-    with open(filename, "r", encoding="utf-8") as fl:
-        myFile = fl.read()
-        count = 0
-        for lines in myFile:
-            line = lines.split()
-            for word in line:
-                for char in word:
-                    count += 1
-    return(count)
+    with open(filename, 'w', encoding="utf-8") as f:
+        chars = f.write(text)
+    return(chars)
