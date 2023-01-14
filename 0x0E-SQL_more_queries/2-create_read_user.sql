cc374950a17f87f10c107d146bdd6e89d00b5e66
Lawal Olanrewaju Israel <61704062+lawallanre00490038@users.noreply.github.com>
2023-01-14 14:15:43 +0100

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,9 +4,9 @@ CREATE DATABASE
     IF NOT EXISTS hbtn_0d_2;
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
+    IDENTIFIED BY user_0d_2_pwd;
 GRANT SELECT
-   ON `hbtn_0d_2`.*
+   ON hbtn_0d_2.*
    TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+   IDENTIFIED BY user_0d_2_pwd;
 FLUSH PRIVILEGES;
